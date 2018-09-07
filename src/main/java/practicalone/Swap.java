f3dd55e02596e208e350feefe67565daa0a769df
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-09-07 07:31:33 -0400

    Add the template version of the Swap class that has TODO entries.

---
+++ src/main/java/practicalone/Swap.java
@@ -0,0 +1,30 @@
+package practicalone;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+// TODO: Add the correct type of comment
+public class Swap {
+
+  /**
+   * Performs the swap of the two provided values.
+   *
+   * @param first the first value; will receive the second value
+   * @param second the second value; will receive the first value
+   */
+  public static void swap(AtomicInteger first, AtomicInteger second) {
+    // TODO: Add the implementation of this method
+  }
+
+  // TODO: Add the correct type of comment
+  public static void main(String[] args) {
+    AtomicInteger dataFirst = new AtomicInteger(10);
+    AtomicInteger dataSecond = new AtomicInteger(20);
+    System.out.println("Values before the swap:");
+    System.out.println("  First value : " + dataFirst.get());
+    System.out.println("  Second value: " + dataSecond.get());
+    Swap.swap(dataFirst, dataSecond);
+    System.out.println("Values after the swap:");
+    System.out.println("  First value : " + dataFirst.get());
+    System.out.println("  Second value: " + dataSecond.get());
+  }
+}
